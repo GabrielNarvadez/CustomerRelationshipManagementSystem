18dfdd0af67c11647573a1876826686921fb64df
GabrielNarvadez <131564153+GabrielNarvadez@users.noreply.github.com>
2024-12-03 20:13:37 +0800

    Done adding tasks and invoice

--- SmartConnect/app/urls.py
+++ SmartConnect/app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'), 
         # Django Admin
     path('admin/', admin.site.urls),
+    path('tasks/', views.tasks, name='tasks'), 
+    path('invoice/', views.invoice, name='invoice'), 
     
     # API Endpoints (app)
     path('api/', include('app.api.urls')),  
--- SmartConnect/app/views.py
+++ SmartConnect/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render 
+
 def index(request): 
-    return render(request, 'app/index.html') 
+    return render(request, 'app/index.html')
+
+def tasks(request): 
+    return render(request, 'app/tasks.html') 
+
+def invoice(request): 
+    return render(request, 'app/invoice.html') 
